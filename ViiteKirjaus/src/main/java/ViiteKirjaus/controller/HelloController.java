b7966bd99f7967be114708a61945d8a365731ba2
Markus A Nousiainen <markusno@dhp-21.cs.helsinki.fi>
2013-03-26 17:10:53 +0200

    controlleri toimii

--- ViiteKirjaus/src/main/java/ViiteKirjaus/controller/HelloController.java
+++ ViiteKirjaus/src/main/java/ViiteKirjaus/controller/HelloController.java
@@ -12,11 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 
     @RequestMapping("index")
-    public void index(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            response.getWriter().write("Hellosdfsdsdf sdf sdf sdf  World!");
-        } catch (IOException ex) {
-            System.out.println("Ei onnistunut!");
-        }
+    public String index() {
+        return "lista";
     }
 }
